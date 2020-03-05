5214d6b48431c1df4fe5f19d36c168c1b9c175a9
Ghazi <ghazitozri@fsm.u-monastir.tn>
2020-03-05 15:01:27 +0100

    Initial Commit

---
+++ Vive Tunisie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vive Tunisie
+//
+//  Created by Ghazi Tozri on 3/5/20.
+//  Copyright © 2020 wimobi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
